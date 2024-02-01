88501755a2142db2fc49b03032b7f1b71410fa1f
imzero9 <skdud000322@naver.com>
2024-02-01 16:07:45 +0900

    📝 Update: UI 짤림 이슈로 인한 인증서 타이틀 텍스트 변경

--- assets/js/event.js
+++ assets/js/event.js
@@ -556,8 +556,8 @@ window.addEventListener('click', (e) => {
 /* 인증서 초기화 */
 const certifList = certifWrap.querySelector('.certif-list');
 const storyChapter = {
-    Introduction: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-    Basic: [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+    'Lv.1': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    'Lv.2': [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
 };
 const setCertifItem = () => {
     Object.keys(storyChapter).forEach((chapter) => {
